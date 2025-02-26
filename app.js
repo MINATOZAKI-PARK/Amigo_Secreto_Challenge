ac73b48a6ecbc6e12db38c0fad14044506c20cd7
Minatozaki <148259896+MINATOZAKI-PARK@users.noreply.github.com>
2025-02-26 13:35:04 -0600

    correccion de funcion agregarAmigo

--- app.js
+++ app.js
@@ -7,15 +7,17 @@ function agregarAmigo() {
     let input = document.getElementById("amigo");
     let name = input.value.trim(); // Captura el valor y quita espacios extra
     // Validar que el nombre no esté vacío
-    if (name.trim() === ""){
+    if (name === ""){
         alert("Por favor, inserte un nombre.");
         return;
     };
     //Agrega el nombre a la lista de amigos
     if (!namesFriends.includes(name)){
             namesFriends.push(name);
+            console.log("Amigo agregado")
     } else {
         alert("El nombre ya existe en la lista.");
+        return;
     };
     // Limpiar el input
     input.value = "";
