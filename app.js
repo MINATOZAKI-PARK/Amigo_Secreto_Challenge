fa7579c0856d3b01415f8e3d9012828008aa8ea2
Minatozaki <148259896+MINATOZAKI-PARK@users.noreply.github.com>
2025-02-26 13:26:20 -0600

    Creacion de funcion agregarAmigos

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let names_friends = [];
-let friends_selected = [];
+let namesFriends = [];
+let friendsSelected = [];
 
-/*function agregarAmigo() {
-
-}*/
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let name = input.value.trim(); // Captura el valor y quita espacios extra
+    // Validar que el nombre no esté vacío
+    if (name.trim() === ""){
+        alert("Por favor, inserte un nombre.");
+        return;
+    };
+    //Agrega el nombre a la lista de amigos
+    if (!namesFriends.includes(name)){
+            namesFriends.push(name);
+    } else {
+        alert("El nombre ya existe en la lista.");
+    };
+    // Limpiar el input
+    input.value = "";
+}
